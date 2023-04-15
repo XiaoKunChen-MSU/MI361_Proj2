cad79752c968db80c985cc0531e0895bc53605bf
XiaoKun Chen <chenx137@msu.edu>
2023-04-15 17:17:50 -0400

    changes5

--- function.js
+++ function.js
@@ -2,3 +2,7 @@ function trackFunc(){
     document.getElementById("trackfunc").style.color="black";
     document.getElementById("trackfunc").style.textDecoration = "underline";
 }
+
+function trackme (){
+    alert ("Sorry, we could not locate the shipment details for this tracking number. Please make sure your number is correct.")
+}
